60df09f8d3c2138711420fbe09772357a622e3dc
Julian Currie <>
2021-05-16 00:54:02 +0700

    added CheetahListText with add/delete hook functions

---
+++ src/style/separator.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const ListSeparator = styled.View`
+  height: 1px;
+  background-color: white;
+`
--- src/view/home.js
+++ src/view/home.js
@@ -1,27 +1,54 @@
 import React, { useState } from 'react';
 
-import { Title, ContentText } from '../style/text';
+import { Title, CheeatahListText } from '../style/text';
 import { Container } from '../style/container';
 import { CheetahInput } from '../style/input';
+import { CheeatahButton } from '../style/button';
 import { FlatList } from 'react-native';
+import { ListSeparator } from '../style/separator';
 
 export default Home = () => {
 
-	const [value, onChangeText] = useState('Useless Multiline Placeholder');
+	const [superheroList, setHeroList] = useState([]);
+	const [name, setName] = useState('');
+	const [power, setPower] = useState('');
+
+	addHero = () => {
+		setHeroList(list => [...list, { name, power, id: Math.random() }])
+	}
+
+	deleteHero = (id) => {
+		let filteredHeroes = superheroList.filter(hero => hero.id !== id)
+		setHeroList(filteredHeroes)
+	}
 
 	return (
 		<Container>
 			<Title>Superheroes</Title>
 			<CheetahInput
 				hint='Name'
+				onChangeText={text => setName(text)}
 			/>
 			<CheetahInput
 				hint='Power'
+				onChangeText={text => setPower(text)}
+			/>
+			<CheeatahButton
+				title="Add"
+				onPress={(text) => addHero(text)}
 			/>
 			<FlatList
-				data={[{ title: 'a' }, { title: 'b' }, { title: 'c' }]}
-				renderItem={({ item }) => <ContentText>{item.title}</ContentText>}
-				keyExtractor={(item) => item.title}
+				data={superheroList}
+				keyExtractor={(item) => item.id}
+				style={{ width: '100%', marginTop: 20, paddingBottom: 10 }}
+				ItemSeparatorComponent={() => <ListSeparator />}
+				renderItem={({ item }) => (
+					<CheeatahListText
+						content={`Name: ${item.name}`}
+						onLongPress={() => deleteHero(item.id)}
+						description={`Power: ${item.power}`}
+					/>
+				)}
 			/>
 		</Container>
 	);
