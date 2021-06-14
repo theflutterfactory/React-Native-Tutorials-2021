1b6e62604c4794d11b0ec0702b9ab07404aebbcf
chriswayg <chriswayg@gmail.com>
2021-06-14 23:12:09 +0800

    added Mobx observer

--- src/view/ListScreen.js
+++ src/view/ListScreen.js
@@ -1,38 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import {observer} from 'mobx-react';
 
-import { CheeatahListText } from '../style/text';
-import { Container } from '../style/container';
-import { FlatList } from 'react-native';
-import { ListSeparator } from '../style/separator';
+import {CheeatahListText} from '../style/text';
+import {Container} from '../style/container';
+import {FlatList} from 'react-native';
+import {ListSeparator} from '../style/separator';
+import {superheroStore} from '../store/superheroes';
 
-export default ListScreen = ({ route }) => {
-	const [superheroList, setHeroList] = useState([]);
-
-	useEffect(() => {
-		setHeroList(route.params.list)
-	}, [])
-
-
-	deleteHero = (id) => {
-		let filteredHeroes = superheroList.filter(hero => hero.id !== id)
-		setHeroList(filteredHeroes)
-	}
-
-	return (
-		<Container>
-			<FlatList
-				data={superheroList}
-				keyExtractor={(item) => item.id}
-				style={{ width: '100%', marginTop: 20, paddingBottom: 10 }}
-				ItemSeparatorComponent={() => <ListSeparator />}
-				renderItem={({ item }) => (
-					<CheeatahListText
-						content={`Name: ${item.name}`}
-						onLongPress={() => deleteHero(item.id)}
-						description={`Power: ${item.power}`}
-					/>
-				)}
-			/>
-		</Container>
-	);
-}
+export default ListScreen = observer(() => {
+  return (
+    <Container>
+      <FlatList
+        data={superheroStore.superheroes}
+        keyExtractor={(item) => item.id}
+        style={{width: '100%', marginTop: 20, paddingBottom: 10}}
+        ItemSeparatorComponent={() => <ListSeparator />}
+        renderItem={({item}) => (
+          <CheeatahListText
+            content={`Name: ${item.name}`}
+            onLongPress={() => superheroStore.deleteHero(item.id)}
+            description={`Power: ${item.power}`}
+          />
+        )}
+      />
+    </Container>
+  );
+});
